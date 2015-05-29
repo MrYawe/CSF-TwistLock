18b5597f7a25946e817777889901a6a871620281
Korgys <j0.eritzian@hotmail.fr>
2015-05-29 15:47:37 +0200

    Adding sockets

---
+++ Fluffy/Fluffy/FSocket.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluffyNet
+{
+    public class FSocket : IFSocket
+    {
+        public string IpAddress
+        {
+            get;
+            set;
+        }
+        public int Port
+        {
+            get;
+            set;
+        }
+
+        public FSocket()
+        {
+            Socket socket = new Socket();
+        }
+
+        public void Connect(string address, int port)
+        {
+
+        }
+
+        public int Send(string value)
+        {
+            return 0;
+        }
+    }
+}
