838fde691b1e2001046e987db5e173145bd94eb0
Korgys <j0.eritzian@hotmail.fr>
2015-05-29 18:27:52 +0200

    Updating main process

--- Fluffy/Fluffy/Program.cs
+++ Fluffy/Fluffy/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Fluffy
@@ -14,12 +15,12 @@ namespace Fluffy
             // VARIABLES
             string teamName = "Comic Sans Fluffy " + new Random().Next(10);
             string server   = "172.30.7.16";
-            int port        = 9867;
+            int port        = 9877;
 
             // TESTING 
-            Console.Write("Server: ");
+            Console.WriteLine("Server: " + server);
             //server = Console.ReadLine();
-            Console.Write("Port: ");
+            Console.WriteLine("Port: " + port);
             //port = int.Parse(Console.ReadLine());
 
             // CONNECTION TO THE SERVER
@@ -30,21 +31,37 @@ namespace Fluffy
             // RECEPTION INFO PLAYER
             Joueur joueur = new Joueur();
             string infoJoueur = socket.Receive();
-            infoJoueur = "Vous êtes le Joueur 1 (ROUGE), attente suite ..."; // TODO: remove
             if (infoJoueur.IndexOf("eur ") > 0)
                 joueur.Id = int.Parse(infoJoueur.Substring(infoJoueur.IndexOf("eur ") + 4, 1));
             else
                 joueur.Id = 2;
+            Console.WriteLine("ID JOUEUR : " + joueur.Id);
 
             // RECEPTION INFO MAP
             Plateau plateau = Plateau.getInstance();
             string infoMap = socket.Receive();
             plateau.Init(infoMap.Split('=')[1]);
-            Console.WriteLine(infoMap);
+            Console.WriteLine("MAP : " + infoMap.Split('=')[1]);
             
             // DEBUT DU JEU
-            string begin = socket.Receive();
-            Console.WriteLine("Infos partie : " + begin);
+            bool isActive = true; // Etat de la partie
+            int turn = joueur.Id;
+            while(isActive)
+            {
+                if (turn == 1)
+                {
+                    // Genere un coup
+                    string coup = "";
+                    socket.Send(coup);
+                    Console.WriteLine("ENVOI : " + coup);
+                }
+                else
+                {
+                    // Reception du coup ennemi
+                    string enCoup = socket.Receive();
+
+                }
+            }
 
             Console.ReadLine();
         }
