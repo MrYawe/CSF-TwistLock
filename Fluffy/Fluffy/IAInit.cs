3018cb349dccb3b8d5b665a88da7cf7fb22fb574
Obiwan1995 <brice.miclo@gmail.com>
2015-05-29 17:55:49 +0200

    Readd IAInit

--- Fluffy/Fluffy/IAInit.cs
+++ Fluffy/Fluffy/IAInit.cs
@@ -8,5 +8,33 @@ namespace Fluffy
 {
     class IAInit
     {
+        public static List<Point> freePoints {get;set;} 
+        public static Conteneur[][] freeConteneurs {get;set;}
+        public IAInit()
+        {
+            freeConteneurs = Plateau.getInstance().conteneurs;
+            SortConteneur sc = new SortConteneur();
+            Array.Sort(freeConteneurs, sc);
+
+            freePoints = Plateau.getInstance().points;
+            SortPoint sp = new SortPoint();
+            freePoints.Sort(sp);
+        }
+    }
+
+    public class SortConteneur : IComparer<Conteneur[]>
+    {
+        public int Compare(Conteneur x, Conteneur y)
+        {
+            return y.valeur - x.valeur;
+        }
+    }
+
+    public class SortPoint : IComparer<Point>
+    {
+        public int Compare(Point x, Point y)
+        {
+            return y.valeur - x.valeur;
+        }
     }
 }
