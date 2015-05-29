17d349207000ad8f4908bc1238f35cd433665783
Korgys <j0.eritzian@hotmail.fr>
2015-05-29 16:29:00 +0200

    Updating main and sockets

--- Fluffy/Fluffy/Program.cs
+++ Fluffy/Fluffy/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FluffyNet;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,34 @@ namespace Fluffy
     {
         static void Main(string[] args)
         {
+            // VARIABLES
+            string teamName = "Comic Sans Fluffy " + new Random().Next(10);
+            string server;
+            int port;
+            bool isServer;
+
+            // TESTING 
+            Console.Write("server (1) or client (2) ? ");
+            isServer = int.Parse(Console.ReadLine()) == 1;
+            Console.Write("server: ");
+            server = Console.ReadLine();
+            Console.Write("port: ");
+            port = int.Parse(Console.ReadLine());
+
+            // CONNECTION TO THE SERVER
+            FSocket socket = new FSocket();
+            socket.Connect(server, port);
+            if(!isServer)
+            {
+                socket.Send(teamName);
+            }
+            else
+            {
+                List<string> teams = new List<string>();
+                string line = socket.Receive();
+
+            }
+
         }
     }
 }
