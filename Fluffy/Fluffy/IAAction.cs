4d2ebd39e1bc28c4289c1061865026de0c87987d
CilagINept <galic@voila.fr>
2015-05-29 17:07:18 +0200

    IAAction
    Plleeeeeeiiiiiinnn ddeeee cchhhoooooosssseeesss

--- Fluffy/Fluffy/IAAction.cs
+++ Fluffy/Fluffy/IAAction.cs
@@ -8,5 +8,79 @@ namespace Fluffy
 {
     class IAAction
     {
+        public bool recieveAction(string action)
+        {
+            bool res = true;
+
+            if(isActionValid(action))
+            {
+                int ligne = int.Parse(action[0].ToString());
+                int colonne = action[1] - 'A' - 1;
+                int point = int.Parse(action[2].ToString());
+
+                Conteneur cont = Plateau.getInstance().conteneurs[ligne][colonne];
+                cont.coins[point].statut = Point.status.ME;
+
+                int pointEnnemi = 0;
+                int pointMoi = 0;
+
+                if (cont.coins[0].statut == Point.status.YOU) { pointEnnemi++; }
+                else if (cont.coins[0].statut == Point.status.ME) { pointMoi++; }
+
+                if (cont.coins[1].statut == Point.status.YOU) { pointEnnemi++; }
+                else if (cont.coins[1].statut == Point.status.ME) { pointMoi++; }
+
+                if (cont.coins[2].statut == Point.status.YOU) { pointEnnemi++; }
+                else if (cont.coins[2].statut == Point.status.ME) { pointMoi++; }
+
+                if (cont.coins[3].statut == Point.status.YOU) { pointEnnemi++; }
+                else if (cont.coins[3].statut == Point.status.ME) { pointMoi++; }
+
+                if (pointEnnemi > pointMoi)
+                {
+                    cont.statut = Conteneur.status.YOU;
+                    cont.coins[0].valeur -= cont.valeur;
+                    cont.coins[1].valeur -= cont.valeur;
+                    cont.coins[2].valeur -= cont.valeur;
+                    cont.coins[3].valeur -= cont.valeur;
+                }
+                else if (pointEnnemi < pointMoi)
+                {
+                    cont.statut = Conteneur.status.ME;
+                    cont.coins[0].valeur += cont.valeur;
+                    cont.coins[1].valeur += cont.valeur;
+                    cont.coins[2].valeur += cont.valeur;
+                    cont.coins[3].valeur += cont.valeur;
+                }
+                else
+                {
+                    cont.statut = Conteneur.status.NONE;
+                }
+            }
+            else
+            {
+                res = false;
+            }
+
+            return res;
+        }
+
+        public bool isActionValid(string action)
+        {
+            bool res;
+
+            res = (action.Length == 3);
+            res = (char.IsLetter(action[1]) && res);
+
+            int ligne = int.Parse(action[0].ToString());
+            int colonne = action[1] - 'A' - 1;
+            int point = int.Parse(action[2].ToString());
+
+            res = (ligne >= 0 && ligne <= Plateau.getInstance().ligne - 1 && res);
+            res = (colonne >= 0 && colonne <= Plateau.getInstance().colonne - 1 && res);
+            res = (point >= 0 && point <= 3 && res);
+
+            return res;
+        }
     }
 }
