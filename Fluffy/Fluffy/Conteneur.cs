6032bffd71cb1d138e634c8420d0fb70c7860796
Yannis Weishaupt <yannis.weishaupt@outlook.com>
2015-05-29 15:57:12 +0200

    Add some class

---
+++ Fluffy/Fluffy/Conteneur.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fluffy
+{
+    class Conteneur
+    {
+        int ligne;
+        int colonne;
+
+        public int ligne
+        {
+            get;
+            set;
+        }
+
+        public int colonne
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Fluffy/Fluffy/Joueur.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fluffy
+{
+    class Joueur
+    {
+        private string id;
+        private string couleur;
+        private int nbTwistLock=20;
+
+
+        public enum status { NONE, ONE, TWO };
+
+
+
+        // Accessor
+        public string id
+        {
+            get;
+            set;
+        }
+
+        public string couleur
+        {
+            get;
+            set;
+        }
+
+        public int nbTwistLock
+        {
+            get;
+            set;
+        }
+    }
+}
