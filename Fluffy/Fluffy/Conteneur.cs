478492fab24f44370fccf732a10356b8952d170a
Jean-Marcellin Truong <mini.fusee@hotmail.Fr>
2015-05-29 17:12:27 +0200

    Modèles FTW TA MERE
    :(

--- Fluffy/Fluffy/Conteneur.cs
+++ Fluffy/Fluffy/Conteneur.cs
@@ -8,24 +8,24 @@ namespace Fluffy
 {
     class Conteneur
     {
-        public enum status { NONE, ME, YOU };
         public int valeur { get; set; }
-        public status statut { get; set; }
+        public Point.status statut { get; set; }
         public Point[] coins { get; set; }
 
         public Conteneur(int ligne, int colonne, int valeur)
         {
-            this.statut = status.NONE;
+            this.statut = Point.status.NONE;
             this.valeur = valeur;
-            int nbcolonne = Plateau.getInstance().colonnes;
+            int nbcolonne = Plateau.getInstance().nbColonnes;
             coins[0] = Plateau.getInstance().points[(ligne*nbcolonne)+colonne];
             coins[1] = Plateau.getInstance().points[(ligne*nbcolonne)+colonne+1];
             coins[2] = Plateau.getInstance().points[((ligne+1)*nbcolonne)+colonne];
             coins[3] = Plateau.getInstance().points[((ligne+1)*nbcolonne)+colonne+1];
-            coins[0].valeur += valeur;
-            coins[1].valeur += valeur;
-            coins[2].valeur += valeur;
-            coins[3].valeur += valeur;
+            for (int i = 0; i < 4; i++)
+            {
+                coins[i].addParent(this);
+                coins[i].valeur += valeur;
+            }
         }
 
     }
--- Fluffy/Fluffy/Point.cs
+++ Fluffy/Fluffy/Point.cs
@@ -11,11 +11,27 @@ namespace Fluffy
         public enum status { NONE, ME, YOU };
         public int valeur { get; set; }
         public status statut { get; set; } 
+        public List<Conteneur> conteneurs { get; set;}
         
         public Point()
         {
             this.statut = status.NONE;
             this.valeur = 0;
+            this.conteneurs = new List<Conteneur>();
+        }
+
+        public void changeStatus(status stat)
+        {
+            this.statut = stat;
+            for (int i = 0; i<conteneurs.Count(); i++)
+            {
+                conteneurs[i].statut = stat;
+            }
+        }
+
+        public void addParent(Conteneur cont)
+        {
+            conteneurs.Add(cont);
         }
     }
 }
