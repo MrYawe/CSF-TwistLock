ca92c308445ba59f3b6c6c3d229cd242b0b6343b
Korgys <j0.eritzian@hotmail.fr>
2015-05-29 16:00:04 +0200

    Updating socket

--- Fluffy/Fluffy/FSocket.cs
+++ Fluffy/Fluffy/FSocket.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,30 +10,71 @@ namespace FluffyNet
 {
     public class FSocket : IFSocket
     {
-        public string IpAddress
+        private string _server;
+        private int _port;
+
+        private Socket Socket
         {
             get;
             set;
         }
+        public string Server
+        {
+            get { return _server; }
+        }
         public int Port
         {
-            get;
-            set;
+            get { return _port; }
         }
 
         public FSocket()
         {
-            Socket socket = new Socket();
         }
 
-        public void Connect(string address, int port)
+        public void Connect(string server, int port)
         {
+            if(string.IsNullOrEmpty(server))
+            {
+                Console.WriteLine("Le serveur est vide !");
+                return;
+            }
 
+            if(port <= 0 || port > 65556)
+            {
+                Console.WriteLine("Port invalide !");
+                return;
+            }
+
+            // Get host related information.
+            _server = server;
+            _port = port;
+            IPHostEntry hostEntry = Dns.GetHostEntry(server);
+
+            // Loop through the AddressList to obtain the supported AddressFamily. This is to avoid
+            // an exception that occurs when the host IP Address is not compatible with the address family
+            // (typical in the IPv6 case).
+            foreach (IPAddress address in hostEntry.AddressList)
+            {
+                IPEndPoint ipe = new IPEndPoint(address, port);
+                Socket tempSocket = new Socket(ipe.AddressFamily, SocketType.Stream, ProtocolType.Udp);
+
+                tempSocket.Connect(ipe);
+
+                if (tempSocket.Connected)
+                {
+                    Socket = tempSocket;
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
         }
 
-        public int Send(string value)
+        public void Send(string value)
         {
-            return 0;
+
         }
     }
 }
